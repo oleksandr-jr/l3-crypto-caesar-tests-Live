399973c7a22487f93a91443bd32534f74ae3516e
Oleksandr <oleksandr.jr@gmail.com>
2023-03-20 16:31:12 +0100

    Logs

---
+++ src/test/java/org/javarush/oleksandr/MainTest.java
@@ -0,0 +1,15 @@
+package org.javarush.oleksandr;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() throws IOException {
+        Main.main(new String[]{});
+    }
+}
